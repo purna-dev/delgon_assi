fcb678932360e22d608cff4eda4fe0a5ffbd29f5
Purna Chandra Saren <65004060+purna-dev@users.noreply.github.com>
2022-02-20 17:25:07 +0530

    Add files via upload

---
+++ js/app.js
@@ -0,0 +1,26 @@
+let btn_contact = document.getElementById('btn_contact');
+let btn_services = document.getElementById('btn_services');
+let btn_about = document.getElementById('btn_about');
+// let btn_contact = document.getElementById('contact');
+// let btn_contact = document.getElementById('contact');
+btn_services.addEventListener('click',()=>{
+
+
+    console.log('dddn')
+  document.getElementById("mySidenav").style.display = "none";
+
+})
+btn_contact.addEventListener('click',()=>{
+
+
+    console.log('dddn')
+  document.getElementById("mySidenav").style.display = "none";
+
+})
+btn_about.addEventListener('click',()=>{
+
+
+    console.log('dddn')
+  document.getElementById("mySidenav").style.display = "none";
+
+})
